b3a9d3a161039aa1807599c4c3a6710f7e1461b1
vickyokrm <vickyokrm@gmail.com>
2018-08-19 19:45:01 +0200

    fix setup wizard tests

--- tests/end-to-end/ui/00-login.js
+++ tests/end-to-end/ui/00-login.js
@@ -2,6 +2,26 @@
 
 import loginPage from '../../pageobjects/login.page';
 import setupWizard from '../../pageobjects/setup-wizard.page';
+import supertest from 'supertest';
+
+import { adminUsername, adminPassword, adminEmail } from '../../data/user.js';
+
+const request = supertest('http://localhost:3000');
+const prefix = '/api/v1/';
+
+function api(path) {
+	return prefix + path;
+}
+
+const credentials = {
+	['X-Auth-Token']: undefined,
+	['X-User-Id']: undefined
+};
+
+const login = {
+	user: adminUsername,
+	password: adminPassword
+};
 
 describe('[Login]', () => {
 	before(()=>{
@@ -69,87 +89,106 @@ describe('[Login]', () => {
 	});
 });
 
+let alreadyExecuted = false;
+
 describe('[Setup Wizard]', () => {
-	before(()=>{
-		setupWizard.login();
-		setupWizard.organizationType.waitForVisible(15000);
+	before((done)=>{
+		//setupWizard.login();
+		//setupWizard.organizationType.waitForVisible(15000);
+
+		//check to see if the setup-wizard already executed.
+		request.post(api('login'))
+			.send(login)
+			.expect('Content-Type', 'application/json')
+			.expect(200)
+			.expect((res) => {
+				credentials['X-Auth-Token'] = res.body.data.authToken;
+				credentials['X-User-Id'] = res.body.data.userId;
+			})
+			.end(() => {
+				request.get(api('settings/Show_Setup_Wizard'))
+					.set(credentials)
+					.expect('Content-Type', 'application/json')
+					.expect(200)
+					.expect((res) => {
+						alreadyExecuted = res.body.value === 'completed';
+					})
+					.end(done);
+			});
+		//login as Admin
+		loginPage.login({ email: adminEmail, password: adminPassword });
 	});
 
-	describe('[Render - Step 1]', () => {
-		it('it should show organization type', () => {
-			setupWizard.organizationType.isVisible().should.be.true;
-		});
+	it('Decide to Run Setup Wizard', () => {
+		if (alreadyExecuted) {
+			describe('Setup Wizard already completed earlier on', () => {
+				it.skip('skip the setup wizard tests', () => {
+					// kept atleast one empty condition to show the execution log on the console.
+				});
+			});
+		} else {
+			describe('[Render - Step 1]', () => {
+				before(() => {
+					browser.pause(1000); // to make sure the wizard screen is visible
+				});
+				it('it should show organization type', () => {
+					setupWizard.organizationType.isVisible().should.be.true;
+				});
 
-		it('it should show organization name', () => {
-			setupWizard.organizationName.isVisible().should.be.true;
-		});
+				it('it should show organization name', () => {
+					setupWizard.organizationName.isVisible().should.be.true;
+				});
 
-		it('it should show industry', () => {
-			setupWizard.industry.isVisible().should.be.true;
-		});
+				it('it should show industry', () => {
+					setupWizard.industry.isVisible().should.be.true;
+				});
 
-		it('it should show size', () => {
-			setupWizard.size.isVisible().should.be.true;
-		});
+				it('it should show size', () => {
+					setupWizard.size.isVisible().should.be.true;
+				});
 
-		it('it should show country', () => {
-			setupWizard.country.isVisible().should.be.true;
-		});
+				it('it should show country', () => {
+					setupWizard.country.isVisible().should.be.true;
+				});
 
-		it('it should show website', () => {
-			setupWizard.website.isVisible().should.be.true;
-		});
+				it('it should show website', () => {
+					setupWizard.website.isVisible().should.be.true;
+				});
 
-		after(() => {
-			setupWizard.goNext();
-		});
-	});
+				after(() => {
+					setupWizard.goNext();
+				});
+			});
 
-	describe('[Render - Step 2]', () => {
-		it('it should show site name', () => {
-			setupWizard.siteName.isVisible().should.be.true;
-		});
+			describe('[Render - Step 2]', () => {
+				it('it should show site name', () => {
+					setupWizard.siteName.isVisible().should.be.true;
+				});
 
-		it('it should show language', () => {
-			setupWizard.language.isVisible().should.be.true;
-		});
+				it('it should show language', () => {
+					setupWizard.language.isVisible().should.be.true;
+				});
 
-		it('it should server type', () => {
-			setupWizard.serverType.isVisible().should.be.true;
-		});
+				it('it should server type', () => {
+					setupWizard.serverType.isVisible().should.be.true;
+				});
 
-		after(() => {
-			setupWizard.goNext();
-		});
-	});
+				after(() => {
+					setupWizard.goNext();
+				});
+			});
 
-	describe('[Render - Step 3]', () => {
-		it('it should have option for registered server', () => {
-			setupWizard.registeredServer.isExisting().should.be.true;
-		});
+			describe('[Render - Final Step]', () => {
+				it('it should render "Go to your workspace button', () => {
+					setupWizard.goToWorkspace.waitForVisible(15000);
+					setupWizard.goToWorkspace.isVisible().should.be.true;
+				});
 
-		it('it should have option for standalone server', () => {
-			setupWizard.standaloneServer.isExisting().should.be.true;
-		});
-
-		it('it should check option for registered server by default', () => {
-			setupWizard.registeredServer.isSelected().should.be.true;
-		});
-
-		after(() => {
-			setupWizard.goNext();
-		});
-	});
-
-	describe('[Render - Final Step]', () => {
-		it('it should render "Go to your workspace button', () => {
-			setupWizard.goToWorkspace.waitForVisible(15000);
-			setupWizard.goToWorkspace.isVisible().should.be.true;
-		});
-
-		after(() => {
-			setupWizard.goToHome();
-		});
+				after(() => {
+					setupWizard.goToHome();
+				});
+			});
+		}
 	});
 
 	after(() => {
