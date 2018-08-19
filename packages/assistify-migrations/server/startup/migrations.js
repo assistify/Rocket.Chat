2e2a5b59bc42479397fb997eca152ac317e7be2c
vickyokrm <vickyokrm@gmail.com>
2018-08-19 20:11:30 +0200

    change collection propertyname for roomtype

--- packages/assistify-migrations/server/startup/migrations.js
+++ packages/assistify-migrations/server/startup/migrations.js
@@ -6,6 +6,7 @@ on startup which migrate data - ignoring the actual version
 
 Meteor.startup(() => {
 	const topics = RocketChat.models.Rooms.findByType('e').fetch();
+	//Update room type and parent room id for request
 	RocketChat.models.Rooms.findByType('r').forEach((request) => {
 		const update = {};
 		if (!update.$set) {
@@ -24,15 +25,41 @@ Meteor.startup(() => {
 		// update requests
 		RocketChat.models.Rooms.update({_id: request._id}, update);
 	});
-	//Update topics
+
+	//Update room type and parent room id for expertises
 	RocketChat.models.Rooms.update({
 		t: 'e'
 	}, {
 		$set: {
-			oldRoomType: 'e', // move the room type as old room type
+			oldType: 'e', // move the room type as old room type
 			t: 'p' // set new room type to private room
 		}
 	}, {
 		multi: true
 	});
+
+	//update subscriptions for requests
+	RocketChat.models.Subscriptions.update({
+		t: 'r'
+	}, {
+		$set: {
+			oldType: 'r',
+			t: 'p'
+		}
+	}, {
+		multi: true
+	});
+
+	//update subscriptions for expertises
+	RocketChat.models.Subscriptions.update({
+		t: 'e'
+	}, {
+		$set: {
+			oldType: 'e',
+			t: 'p'
+		}
+	}, {
+		multi: true
+	});
+
 });
