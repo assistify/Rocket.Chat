741e26ae8140d96ed18db7bd208ab445b3b46de7
Oliver Jägle <github@beimir.net>
2018-08-25 16:57:22 +0200

    Provide default Assistify-specific configuration of Rocket.Chat

---
+++ packages/assistify-configuration/package.js
@@ -0,0 +1,18 @@
+Package.describe({
+	name: 'assistify:configuration',
+	version: '0.0.2',
+	summary: 'Default configuration of Rocket.Chat for Assistify',
+	git: '',
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+	api.use('ecmascript');
+	api.use('rocketchat:lib');
+	api.use('rocketchat:authorization');
+	api.use('assistify:help-request');
+	api.use('assistify:ai');
+
+	api.addFiles('server/config.js', 'server');
+	api.addFiles('server/roles.js', 'server');
+});
