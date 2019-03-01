caed703915490a00950890b44cc9fac3b8737ecf
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-03-01 18:45:15 +0100

    fix message popup test scripts

--- tests/end-to-end/ui/14-message-popup.js
+++ tests/end-to-end/ui/14-message-popup.js
@@ -1,14 +1,15 @@
-import { adminEmail, adminPassword } from '../../data/user.js';
-
+/* eslint-env mocha */
 import {
 	api,
 	request,
 	getCredentials,
 	credentials,
 } from '../../data/api-data.js';
-import loginPage from '../../pageobjects/login.page';
 import sideNav from '../../pageobjects/side-nav.page';
 import mainContent from '../../pageobjects/main-content.page';
+
+import { username, password, email } from '../../data/user.js';
+import { checkIfUserIsValid } from '../../data/checks';
 
 const users = new Array(10).fill(null)
 	.map(() => `${ Date.now() }.${ Math.random().toString(36).slice(2) }`)
@@ -64,13 +65,6 @@ const createTestUser = async({ email, name, username, password, isMentionable })
 describe('[Message Popup]', () => {
 	describe('test user mentions in message popup', () => {
 		before(() => {
-			browser.executeAsync((done) => {
-				const user = Meteor.user();
-				if (!user) {
-					return done();
-				}
-				Meteor.logout(done);
-			});
 
 			browser.call(async() => {
 				for (const user of users) {
@@ -78,8 +72,9 @@ describe('[Message Popup]', () => {
 				}
 			});
 
-			loginPage.open();
-			loginPage.login({ email: adminEmail, password: adminPassword });
+			checkIfUserIsValid(username, email, password);
+
+			sideNav.openChannel('general');
 
 			sideNav.general.waitForVisible(5000);
 			sideNav.general.click();
