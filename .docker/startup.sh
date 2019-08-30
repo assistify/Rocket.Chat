5b54612f0fa39afd13214a15df98ef0afb507942
mrsimpson <github@beimir.net>
2019-08-30 10:56:15 +0200

    Docker on ECS: use metadata-API as fallback

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -1,3 +1,13 @@
 # in a container environment, we may need to determine the INSTANCE_IP
 # for each container so that uploads are working even if load-balanced in a group
-[ -z $INSTANCE_IP ] && $(hostname -i) node main.js || node main.js
+if [ -z $INSTANCE_IP ]
+then 
+    export INSTANCE_IP=$(hostname -i) 
+fi
+
+if [ -z $INSTANCE_IP ]
+then 
+    export INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 --connect-timeout 2) 
+fi
+
+node main.js
