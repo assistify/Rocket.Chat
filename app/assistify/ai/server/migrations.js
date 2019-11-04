0bcd180d76d0a879c54811810bd69e39434e3670
mrsimpson <github@beimir.net>
2019-11-04 13:47:07 +0100

    Add todo: remove setting

--- app/assistify/ai/server/migrations.js
+++ app/assistify/ai/server/migrations.js
@@ -3,3 +3,5 @@ Up to now, there's no "DB version" stored for assistify.
 Until we've got expensive of contradicting migrations, we'll just use this file to write functions running
 on startup which migrate data - ignoring the actual version
  */
+
+// TODO: Remove setting Livechat_agents_alias
